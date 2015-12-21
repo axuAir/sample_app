dddb82745fb189ac995de023cf40d9824da10e52
axuair <xujie@rd.tuan800.com>
2015-12-21 10:01:02 +0000

    Make a basic User model (including secure passwords)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
+    assert_select 'title', "Sign up | Ruby on Rails Tutorial Sample App"
     assert_response :success
   end
 
