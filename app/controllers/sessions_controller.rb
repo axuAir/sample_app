e9fe93f3dbce23655e6b70e56bf430b1670516ac
axuair <xujie@rd.tuan800.com>
2016-01-05 07:02:31 +0000

    Finish user edit,update,index,and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,22 @@
 class SessionsController < ApplicationController
-  
+
+#登录页面
   def new
   end
-  
+#完成登录操作
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
 
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
-  
+# 删除会话，退出用户
   def destroy
     log_out if logged_in?
     redirect_to root_url
